825f867f9dbbb910afe4b799935c0c8c4d183af5
Steven Kane <kanesteven@gmail.com>
2020-07-10 22:56:31 -0500

    Added interface ILightStrikeable and added methods to LightSink

--- Assets/Scripts/LightSink.cs
+++ Assets/Scripts/LightSink.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class LightSink : MonoBehaviour {
+public class LightSink : MonoBehaviour, ILightStrikeable {
   Light enabledLight;
+
+  public int BeamStrikesThisFrame = 0;
 
   void Start() {
     enabledLight = gameObject.GetComponentInChildren<Light>();
     enabledLight.enabled = false;
+  }
+
+  public List<LightBeam> ComputeOutgoingLightBeams(LightBeam input) {
+    return new List<LightBeam>();
+  }
+
+  public void OnCollide(LightBeam lb) {
+    BeamStrikesThisFrame++;
   }
 }
