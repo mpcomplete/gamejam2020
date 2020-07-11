389187a6dcde397727a594eebd88e22f20546b9f
Matt Perry <mpcomplete@gmail.com>
2020-07-11 18:08:21 -0400

    Better emitter behavior

--- Assets/Scripts/LightSource.cs
+++ Assets/Scripts/LightSource.cs
@@ -3,22 +3,19 @@ using System.Linq;
 using UnityEngine;
 
 public class LightSource : LightStrikeableBase {
-  readonly LightBeamColor[] colors = { LightBeamColor.red, LightBeamColor.green, LightBeamColor.blue };
-  int[] headings = { 1, 2, 3 };
+  public LightBeamColor[] Beams = { LightBeamColor.red, LightBeamColor.green, LightBeamColor.green };
+  public int Heading = 0;
 
   public override List<LightBeam> ComputeOutgoingLightBeams(LightBeam input) {
     if (input == null) {
       // Only generate beams on start, not on collision.
-      return Enumerable.Range(0, 3).Select(i => new LightBeam { Color = colors[i], Heading = headings[i] }).ToList();
+      return Beams.Select(beam => new LightBeam { Color = beam, Heading = Heading }).ToList();
     }
     return new List<LightBeam>();
   }
 
   public override void OnQuarterBeat(int counter) {
     if (counter%4 == 0)
-      headings[0] = (headings[0]+1) % 8;
-    if (counter%4 == 0 || counter%4 == 1)
-      headings[1] = (headings[1]+1) % 8;
-    headings[2] = (headings[2]+1) % 8;
+      Heading = (Heading+1) % 8;
   }
 }
