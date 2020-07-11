717a11b3b2e90cb838f9e385cb52553ab4f197af
Matt Perry <mpcomplete@gmail.com>
2020-07-10 22:14:09 -0400

    Use LightBeam

--- Assets/Scripts/LightBeam.cs
+++ Assets/Scripts/LightBeam.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 
 [System.Serializable]
-public class LightBeam
-{
-    public Color Color = Color.black;
-    public int Heading = 0;
+public class LightBeam {
+  public Color Color = Color.black;
+  public int Heading = 0;
+
+  public LightBeam(Color color, int heading) {
+    Color = color;
+    Heading = heading;
+  }
 }
--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -1,32 +1,22 @@
 ﻿using UnityEngine;
 
-public class Beam {
-  public int heading; // 
-  public int color; // 0-2
-
-  public Beam(int heading, int color) {
-    this.heading = heading;
-    this.color = color;
-  }
-}
-
 public class Mirror : MonoBehaviour {
   // 0-15, 0 is forward, n is n/16th of a revolution around.
   private int orientation {
     get => (int)((transform.eulerAngles.y+.1) * 16f / 360f);
   }
 
-  public Beam[] OnCollide(Beam input) {
+  public LightBeam[] OnCollide(LightBeam input) {
     // Reflections for a mirror with orientation "0".
     // We "rotate" the input beam by our orientation, then find the heading
     // outputOrientation = reflectionMap[inputOrientation] for that mirror.
-    int inputOrientation = input.heading * 2; // heading is 0-7, orientation is 0-15
+    int inputOrientation = input.Heading * 2; // heading is 0-7, orientation is 0-15
     int[] reflectionMap = { 8, 5, 6, 7, -1, 1, 2, 3, 0, 13, 14, 15, -1, 9, 10, 11 };
     int adjustedInput = (inputOrientation + (int)this.orientation) % 16;
     int outputOrientation = reflectionMap[adjustedInput];
     if (outputOrientation != -1) {
       int adjustedOutput = (outputOrientation - (int)this.orientation) % 16;
-      return new Beam[] { new Beam(adjustedOutput / 2, input.color) };
+      return new LightBeam[] { new LightBeam(input.Color, adjustedOutput / 2) };
     }
     return null;
   }
