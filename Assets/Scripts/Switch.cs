7c07e72617ff817f4c7a27b3a972c42044f3848d
Matt Perry <mpcomplete@gmail.com>
2020-07-11 20:18:47 -0400

    Switch stub

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnSwitchTriggered(bool enabled);
+
+public class Switch : LightStrikeableBase {
+  public OnSwitchTriggered OnSwitchTriggered = null;
+
+  public override void OnCollide(LightBeam input) {
+    HandleSwitchTriggered(true);
+  }
+
+  void HandleSwitchTriggered(bool enabled) {
+    if (enabled)
+      GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", Color.yellow * 1.8f);
+    else
+      GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", Color.black);
+    //OnSwitchTriggered(enabled);
+  }
+}
